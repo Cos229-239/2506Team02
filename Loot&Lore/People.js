ec1708647e6d23980d72aae08812f0f9b04c884d
MonkMoonlight <bguntle@gmail.com>
2025-06-16 02:27:32 -0400

    06-16 peopledropbox

---
+++ Loot&Lore/People.js
@@ -0,0 +1,20 @@
+//Monster class
+export class People {
+  constructor({
+    name,
+    classes,
+    race,
+    stats,
+    abilities,
+    background,
+    alignment
+  }) {
+    this.name = name;
+    this.classes = classes;
+    this.race = race;
+    this.stats = stats;
+    this.abilities = abilities;
+    this.background = background;
+    this.alignment = alignment;
+  }
+}
