8638ffe999ad936ba952b754e4161b092b076b79
MonkMoonlight <bguntle@gmail.com>
2025-06-17 15:24:02 -0400

    6-17 people generater 2

--- Loot&Lore/App.js
+++ Loot&Lore/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 //import ChatScreen from './ChatScreen';
-import Test from './PeopleGenerater';
+import Test from './PeopleCard';
 import NewPage from './PeopleDropbox';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
+//const Stack = createStackNavigator();
 
 export default function App() {
   return (
-     <NavigationContainer>
-      <Stack.Navigator initialRouteName="ChatScreen">
-        <Stack.Screen name="ChatScreen" component={NewPage} />
-        <Stack.Screen name="NewPage" component={Test} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    //  <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="Wall">
+    //     <Stack.Screen name="Wall" component={NewPage} />
+    //     <Stack.Screen name="NewPage" component={Test} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <NewPage/>
 
   );
 }
