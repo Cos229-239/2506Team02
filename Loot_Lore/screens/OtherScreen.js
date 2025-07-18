f9f3f43326e15f49a882048588a9ff31be984389
MonkMoonlight <bguntle@gmail.com>
2025-07-17 21:06:19 -0400

    2025/07/17 OtherScreen visability fixed

--- Loot_Lore/screens/OtherScreen.js
+++ Loot_Lore/screens/OtherScreen.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */ 
 import React, { useContext } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import { auth } from '../firebaseConfig'; 
 import { signOut } from 'firebase/auth'; 
 
@@ -22,6 +22,12 @@ export default function OtherScreen({ navigation }) {
   };
 
   return (
+    <SafeAreaView style={globalStyles.screen}>
+       <ScrollView
+                contentContainerStyle={styles.scroll}
+                keyboardShouldPersistTaps="handled"
+              >
+
     <View style={[globalStyles.screen, styles.container, { backgroundColor: themeColors.background }]}>
       <Text style={[styles.title, { color: themeColors.text }]}>Loot & Lore</Text>
       <Image source={require('../assets/logo.png')} style={styles.logo} />
@@ -58,6 +64,8 @@ export default function OtherScreen({ navigation }) {
         </TouchableOpacity>
       </View>
     </View>
+     </ScrollView>
+    </SafeAreaView>
   );
 }
 
