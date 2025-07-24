a3998445cea0de361549976d0cdee7b70207418e
Sammy Bouchefaa <SMBouchefaa@student.fullsail.edu>
2025-07-23 19:39:38 -0500

    Minor changes done to the SignUpScreen.js page.

--- Loot_Lore/screens/SignUpScreen.js
+++ Loot_Lore/screens/SignUpScreen.js
@@ -319,7 +319,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: 'rgba(0, 0, 0, 1)',
     padding: 20,
   },
   modalTitle: {
